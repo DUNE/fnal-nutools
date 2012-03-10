87d31e981c0fcf9188842eae828fb9958c0286de
bckhouse <bckhouse@692d5501-4a7f-4c20-9605-99b1da982744>
2012-03-10 06:57:39 +0000

    Same cacheing changes as to View2D.
    git-svn-id: http://cdcvs.fnal.gov/subversion/nusoftsvn/trunk/nutools@199 692d5501-4a7f-4c20-9605-99b1da982744

--- EventDisplayBase/View3D.cxx
+++ EventDisplayBase/View3D.cxx
@@ -5,52 +5,44 @@ using namespace evdb;
 
 //......................................................................
 
-View3D::View3D() 
-{ 
-  fNextMarker3DBox  = fMarker3DBoxL.end();
-  fNextPolyMarker3D = fPolyMarker3DL.end();
-  fNextPolyLine3D   = fPolyLine3DL.end();
-  fNextText3D       = fText3DL.end();
+std::list<TMarker3DBox*>  View3D::fgMarker3DBoxL;
+std::list<TPolyMarker3D*> View3D::fgPolyMarker3DL;
+std::list<TPolyLine3D*>   View3D::fgPolyLine3DL;
+std::list<TText*>         View3D::fgText3DL;
 
-  // Add one object of every type to get lists started
-  this->AddMarker3DBox(0.0,0.0,0.0,50.0,50.0,50.0,0.0,0.0);
-  this->AddPolyMarker3D(1,2,24,1.0);
-  this->AddPolyLine3D(1,2,5,1);
-  this->AddText(0.0,0.0,"");
-  this->Clear();
+//......................................................................
+
+View3D::View3D() 
+{
 }
 
 //......................................................................
 
 View3D::~View3D() 
 {
-  for_each(fMarker3DBoxL.begin(), fMarker3DBoxL.end(), delete_marker3dbox());
-  for_each(fPolyMarker3DL.begin(),fPolyMarker3DL.end(),delete_polymarker3d());
-  for_each(fPolyLine3DL.begin(),  fPolyLine3DL.end(),  delete_polyline3d());
-  for_each(fText3DL.begin(),      fText3DL.end(),      delete_text());
+  // Make sure to return all our objects to where they came from
+  Clear();
 }
 
 //......................................................................
 
 void View3D::Draw()
 {
-  for_each(fMarker3DBoxL.begin(), fNextMarker3DBox, draw_tobject());
-  for_each(fPolyMarker3DL.begin(),fNextPolyMarker3D,draw_tobject());
-  for_each(fPolyLine3DL.begin(),  fNextPolyLine3D,  draw_tobject());
-  for_each(fText3DL.begin(),      fNextText3D,      draw_tobject());
+  for_each(fMarker3DBoxL.begin(), fMarker3DBoxL.end(), draw_tobject());
+  for_each(fPolyMarker3DL.begin(),fPolyMarker3DL.end(),draw_tobject());
+  for_each(fPolyLine3DL.begin(),  fPolyLine3DL.end(),  draw_tobject());
+  for_each(fText3DL.begin(),      fText3DL.end(),      draw_tobject());
 }
 
 //......................................................................
 
 void View3D::Clear() 
 {
-  // To clear, just reset the next positions to use for inserts to the
-  // begining of the lists. Keep all the memory around as it it very likely
-  // we will reuse it
-  fNextMarker3DBox  = fMarker3DBoxL.begin();
-  fNextPolyMarker3D = fPolyMarker3DL.begin();
-  fNextPolyLine3D   = fPolyLine3DL.begin();
-  fNextText3D       = fText3DL.begin();
+  // Empty each of our lists, appending them back onto the static ones
+  fgMarker3DBoxL.splice(fgMarker3DBoxL.end(), fMarker3DBoxL);
+  fgPolyMarker3DL.splice(fgPolyMarker3DL.end(), fPolyMarker3DL);
+  fgPolyLine3DL.splice(fgPolyLine3DL.end(), fPolyLine3DL);
+  fgText3DL.splice(fgText3DL.end(), fText3DL);
 }
 
 //......................................................................
@@ -59,22 +51,21 @@ TMarker3DBox& View3D::AddMarker3DBox(double x,  double y,  double z,
 					double dx, double dy, double dz,
 					double th, double ph)
 {
+  // See comment in View2D::AddMarker()
   TMarker3DBox* m = 0;
-  if (fNextMarker3DBox == fMarker3DBoxL.end()) {
-    // Grow the list...
+  if(fgMarker3DBoxL.empty()){
     m = new TMarker3DBox(x,y,z,dx,dy,dz,th,ph);
     m->SetBit(kCanDelete,kFALSE);
-    fMarker3DBoxL.push_back(m);
-    fNextMarker3DBox = fMarker3DBoxL.end();
   }
   else {
-    // Reuse the marker at the current position
-    m = *fNextMarker3DBox;
+    m = fgMarker3DBoxL.back();
+    fgMarker3DBoxL.pop_back();
+
     m->SetPosition(x,y,z);
     m->SetSize(dx,dy,dz);
-    ++fNextMarker3DBox;
   }
-  // Return the marker just added so users can twiddle it
+
+  fMarker3DBoxL.push_back(m);
   return *m;
 }
 
@@ -83,30 +74,28 @@ TMarker3DBox& View3D::AddMarker3DBox(double x,  double y,  double z,
 TPolyMarker3D& View3D::AddPolyMarker3D(int n, int c, int st, double sz)
 {
   TPolyMarker3D* pm = 0;
-  if (fNextPolyMarker3D == fPolyMarker3DL.end()) {
-    // Grow the list...
+  if(fgPolyMarker3DL.empty()){
     pm = new TPolyMarker3D(n);
     pm->SetBit(kCanDelete,kFALSE);
     pm->SetMarkerColor(c);
     pm->SetMarkerStyle(st);
     pm->SetMarkerSize(sz);
-    fPolyMarker3DL.push_back(pm);
-    fNextPolyMarker3D = fPolyMarker3DL.end();
   }
   else {
-    // Reuse the polymarker at the current position
-    // the first call to SetPolyMarker3D with the 0
+    pm = fgPolyMarker3DL.back();
+    fgPolyMarker3DL.pop_back();
+
+    // The first call to SetPolyMarker3D with the 0
     // deletes the current set of points before trying
     // to make a new set
-    pm = *fNextPolyMarker3D;
     pm->SetPolyMarker(0,(double*)0,1,"");
     pm->SetPolyMarker(n,(double*)0,1,"");
     pm->SetMarkerColor(c);
     pm->SetMarkerStyle(st);
     pm->SetMarkerSize(sz);
-    ++fNextPolyMarker3D;
   }
-  // Return the marker just added so users can twiddle it (ie. add points)
+
+  fPolyMarker3DL.push_back(pm);
   return *pm;
 }
 
@@ -115,30 +104,28 @@ TPolyMarker3D& View3D::AddPolyMarker3D(int n, int c, int st, double sz)
 TPolyLine3D& View3D::AddPolyLine3D(int n, int c, int w, int s)
 {
   TPolyLine3D* pl = 0;
-  if (fNextPolyLine3D == fPolyLine3DL.end()) {
-    // Grow the list...
+  if(fgPolyLine3DL.empty()){
     pl = new TPolyLine3D(n);
     pl->SetBit(kCanDelete,kFALSE);
     pl->SetLineColor(c);
     pl->SetLineWidth(w);
     pl->SetLineStyle(s);
-    fPolyLine3DL.push_back(pl);
-    fNextPolyLine3D = fPolyLine3DL.end();
   }
   else {
-    // Reuse the polyline at the current position
-    // the first call to SetPolyMarker3D with the 0
+    pl = fgPolyLine3DL.back();
+    fgPolyLine3DL.pop_back();
+
+    // The first call to SetPolyMarker3D with the 0
     // deletes the current set of points before trying
     // to make a new set
-    pl = *fNextPolyLine3D;
     pl->SetPolyLine(0,(double*)0,"");
     pl->SetPolyLine(n,(double*)0,"");
     pl->SetLineColor(c);
     pl->SetLineWidth(w);
     pl->SetLineStyle(s);
-    ++fNextPolyLine3D;
   }
-  // Return the marker just added so users can twiddle it (ie. add points)
+
+  fPolyLine3DL.push_back(pl);
   return *pl;
 }
 
@@ -147,21 +134,18 @@ TPolyLine3D& View3D::AddPolyLine3D(int n, int c, int w, int s)
 TText& View3D::AddText(double x, double y, const char* text)
 {
   TText* itxt = 0;
-  if (fNextText3D == fText3DL.end()) {
-    // Grow the list...
+  if(fgText3DL.empty()){
     itxt = new TText(x,y,text);
     itxt->SetBit(kCanDelete,kFALSE);
-    fText3DL.push_back(itxt);
-    fNextText3D = fText3DL.end();
   }
   else {
-    // Reuse the text at the current position
-    itxt = *fNextText3D;
+    itxt = fgText3DL.back();
+    fgText3DL.pop_back();
+
     itxt->SetText(x,y,text);
-    ++fNextText3D;
   }
 
-  // Return the marker just added so users can twiddle it
+  fText3DL.push_back(itxt);
   return *itxt;
 }
 
--- EventDisplayBase/View3D.h
+++ EventDisplayBase/View3D.h
@@ -2,7 +2,7 @@
 /// \file  View3D.h
 /// \brief A collection of 3D drawable objects
 /// 
-/// \version $Id: View3D.h,v 1.1.1.1 2010-12-22 16:18:52 p-nusoftart Exp $
+/// \version $Id: View3D.h,v 1.2 2012-03-10 06:57:39 bckhouse Exp $
 /// \author  messier@indiana.edu
 ////////////////////////////////////////////////////////////////////////
 #ifndef EVDB_VIEW3D_H
@@ -31,15 +31,18 @@ namespace evdb {
     TText&         AddText(double x, double y, const char* text);
     
   private:
+    // Shared pool of unused objects. Any instance may take one for its own
+    // purposes. This is the same scheme as used by View2D. See further
+    // description there.
+    static std::list<TMarker3DBox*>  fgMarker3DBoxL;
+    static std::list<TPolyMarker3D*> fgPolyMarker3DL;
+    static std::list<TPolyLine3D*>   fgPolyLine3DL;
+    static std::list<TText*>         fgText3DL;
+
     std::list<TMarker3DBox*>  fMarker3DBoxL;  //!< List of 3D marker boxes
     std::list<TPolyMarker3D*> fPolyMarker3DL; //!< List of poly markers
     std::list<TPolyLine3D*>   fPolyLine3DL;   //!< List of poly lines
     std::list<TText*>         fText3DL;       //!< List of texts
-    
-    std::list<TMarker3DBox*>::iterator  fNextMarker3DBox; //!< Top of the list
-    std::list<TPolyMarker3D*>::iterator fNextPolyMarker3D;//!< Top of the list
-    std::list<TPolyLine3D*>::iterator   fNextPolyLine3D;  //!< Top of the list
-    std::list<TText*>::iterator         fNextText3D;      //!< Top of the list
   };
 }
 
