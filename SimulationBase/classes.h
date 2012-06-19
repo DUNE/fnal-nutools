e3a81205bb4920288bdcb1e973aef0117ae168e1
brebel <brebel@692d5501-4a7f-4c20-9605-99b1da982744>
2012-06-19 23:17:54 +0000

    add in some more lines necessary to define the associations
    git-svn-id: http://cdcvs.fnal.gov/subversion/nusoftsvn/trunk/nutools@221 692d5501-4a7f-4c20-9605-99b1da982744

--- SimulationBase/classes.h
+++ SimulationBase/classes.h
@@ -1,9 +1,9 @@
 //
 // Build a dictionary.
 //
-// $Id: classes.h,v 1.4 2012-06-19 23:03:07 bckhouse Exp $
-// $Author: bckhouse $
-// $Date: 2012-06-19 23:03:07 $
+// $Id: classes.h,v 1.5 2012-06-19 23:17:54 brebel Exp $
+// $Author: brebel $
+// $Date: 2012-06-19 23:17:54 $
 // 
 // Original author Rob Kutschke, modified by klg
 //
@@ -26,16 +26,22 @@
 // Only include objects that we would like to be able to put into the event.
 // Do not include the objects they contain internally.
 //
+
 template class std::vector<simb::MCNeutrino>;
 template class std::vector<simb::MCTrajectory>;
 template class std::vector<simb::MCParticle>;
 template class std::vector<simb::MCTruth>;
 template class std::vector<simb::MCFlux>;
 
+template class std::pair< art::Ptr<simb::MCFlux>,  art::Ptr<simb::MCTruth> >;
+template class std::pair< art::Ptr<simb::MCTruth>, art::Ptr<simb::MCFlux>  >;
+template class art::Assns<simb::MCFlux,  simb::MCTruth, void>;
+template class art::Assns<simb::MCTruth, simb::MCFlux,  void>;
+
 template class art::Wrapper< std::vector<simb::MCNeutrino> >;
 template class art::Wrapper< std::vector<simb::MCTrajectory> >;
 template class art::Wrapper< std::vector<simb::MCParticle> >;
 template class art::Wrapper< std::vector<simb::MCTruth> >;
 template class art::Wrapper< std::vector<simb::MCFlux> >;
-template class art::Wrapper< art::Assns<simb::MCTruth, simb::MCFlux, void> >;
-template class art::Wrapper< art::Assns<simb::MCFlux, simb::MCTruth, void> >;
+template class art::Wrapper< art::Assns<simb::MCFlux,  simb::MCTruth, void> >;
+template class art::Wrapper< art::Assns<simb::MCTruth, simb::MCFlux,  void> >;
