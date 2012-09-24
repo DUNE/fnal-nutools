9e28b598be376edcd4f90ca3db10ae3cda60a8c7
brebel <brebel@692d5501-4a7f-4c20-9605-99b1da982744>
2012-09-24 19:38:53 +0000

    inline the function to find the random random seed
    git-svn-id: http://cdcvs.fnal.gov/subversion/nusoftsvn/trunk/nutools@291 692d5501-4a7f-4c20-9605-99b1da982744

--- EventGeneratorBase/evgenbase.h
+++ EventGeneratorBase/evgenbase.h
@@ -1,6 +1,8 @@
 #ifndef EVGENBASE_H
 #define EVGENBASE_H
 
+#include "TRandom3.h"
+
 /// Physics generators for neutrinos, cosmic rays, and others
 namespace evgb {
   /// Enumerate mother codes for primary particles. 
@@ -16,4 +18,20 @@ namespace evgb {
 
   unsigned int GetRandomNumberSeed();
 }
+
+inline unsigned int evgb::GetRandomNumberSeed()
+{
+
+  // the maximum allowed seed for the art::RandomNumberGenerator
+  // is 900000000. Use the system random number generator to get a pseudo-random
+  // number for the seed value, and take the modulus of the maximum allowed 
+  // seed to ensure we don't ever go over that maximum
+  
+  // Set gRandom to be a TRandom3 based on this state in case we need to pull
+  // random values from histograms, etc
+  TRandom3 *rand = new TRandom3(0);
+    gRandom = rand;
+    return rand->Integer(900000000);
+}
+
 #endif
